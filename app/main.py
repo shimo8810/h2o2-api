1094fdfed1e163b2d4656357c6cb14951499ff7a
shimo8810 <shimo.8810@gmail.com>
2025-11-19 21:08:14 +0900

    feat: init commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import logging
+
+from fastapi import FastAPI
+from mangum import Mangum
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+app = FastAPI()
+handler = Mangum(app)
+
+
+@app.get("/")
+async def root() -> dict[str, str]:
+    return {"message": "Hello, World!!"}
