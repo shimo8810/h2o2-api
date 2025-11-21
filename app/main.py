ce84ea7b5dfee6ef75f67281671d2a0c825bde1c
shimo8810 <shimo.8810@gmail.com>
2025-11-21 22:13:13 +0900

    feat: reorganize imports and include router in main application

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-import logging
-
 from fastapi import FastAPI
 from mangum import Mangum
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+from .routers import users
 
 app = FastAPI()
-handler = Mangum(app)
+app.include_router(users.router)
 
 
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello, World!!"}
+
+
+handler = Mangum(app)
